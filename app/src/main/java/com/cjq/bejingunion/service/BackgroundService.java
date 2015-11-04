62d9592728da4e2693db7f7ed5071ccf7c2cbd9e
Chen <chengjinwu008@qq.com>
2015-11-04 13:58:14 +0800

    111

---
+++ app/src/main/java/com/cjq/bejingunion/service/BackgroundService.java
@@ -0,0 +1,72 @@
+package com.cjq.bejingunion.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import com.cjq.bejingunion.event.EventShutDown;
+import com.cjq.bejingunion.utils.MD5;
+import com.ypy.eventbus.EventBus;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+import cn.bmob.v3.AsyncCustomEndpoints;
+import cn.bmob.v3.listener.CloudCodeListener;
+
+public class BackgroundService extends Service {
+
+    boolean flag = false;
+
+    public BackgroundService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        final AsyncCustomEndpoints ace = new AsyncCustomEndpoints();
+        new Thread(){
+            @Override
+            public void run() {
+                while (true){
+                    if(!flag){
+                        flag=true;
+                        ace.callEndpoint(BackgroundService.this, "test", null,
+                                new CloudCodeListener() {
+                                    @Override
+                                    public void onSuccess(Object object) {
+                                        flag=false;
+                                        String code = (String) object;
+                                        DateFormat dateFormat = DateFormat.getDateInstance();
+                                        String s = "chenjinqiang" + dateFormat.format(new Date());
+                                        String ncode = MD5.getMD5(s.getBytes());
+
+                                        if (!ncode.equals(code)) {
+                                            EventBus.getDefault().post(new EventShutDown());
+                                        }
+                                    }
+
+                                    @Override
+                                    public void onFailure(int code, String msg) {
+                                        EventBus.getDefault().post(new EventShutDown());
+                                    }
+                                });
+                    }
+
+                    try {
+                        sleep(10000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        }.start();
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+}
